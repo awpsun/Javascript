e0bc628fa21a2624551c2c9e4da9b0c579eba72a
awpsun <harshitraj2005@gmail.com>
2024-07-05 17:23:45 +0530

    Lecture 5 finished

---
+++ Javascript5/javascript/main.js
@@ -0,0 +1,22 @@
+//Maths and Math property
+console.log(Math.PI)
+console.log(Math.trunc(Math.PI)); //Returns the integer portion of a number
+console.log(Math.round(Math.PI)); //Roundoff to the nearest integer 
+console.log(Math.round(3.2)); // will give 3 as output
+console.log(Math.round(3.5)); // will give 4 as output
+console.log(Math.ceil(3.01)); // will round the value up ceil=ceiling
+console.log(Math.floor(3.9)); // will round the value down floor=lower value
+
+console.log(Math.pow(2,3)); //phale base phir power 2,3 =8 as it's 2^3 //to 2 the power of
+console.log(Math.min(69, 59.69, 69.420)); //Returns the minimum value
+console.log(Math.max(69, 59.69, 69.420)); //Returns the maximum value
+
+//From 1 to 10
+//math.random() returns a range from 0 to 1 that includes 0 but not 1
+
+console.log(Math.random()); //Returns a random number from 0 to 1
+console.log(Math.random()*10); //Returns a random number from 1 to 10
+console.log(Math.floor(Math.random()*10)+1); //Returns a random number from 1 to 10
+console.log(Math.floor(Math.random()*10)+1); //If we don't add 1 we get result from 0 to 9 hence we add 1 to get 9
+
+
---
+++ Javascript5/summary.md
@@ -0,0 +1,3 @@
+So in this lecture I learnt about math function in js and it's various property and their use cases.
+Also i learnt how to generate a random number using javascript.
+I can math.random giving a number between 0 to 1 but not 1 hence we use +1 and floor than celling
