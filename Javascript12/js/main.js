978aec0dd993946557c8046c49c40f74b4fdf672
awpsun <harshitraj2005@gmail.com>
2024-07-08 15:07:45 +0530

    Finished Lecture 12 (loops)

---
+++ Javascript12/js/main.js
@@ -0,0 +1,35 @@
+//LOOP
+//while loop
+let aNumber = 10;
+while (aNumber <=10){
+    aNumber+=2;
+    console.log(aNumber);
+  
+}
+//do loop
+let hisNumber = 100;
+do { //do runs the code atleast 1 times where as while only runs if it can run more than 1time
+    hisNumber+=2;
+    console.log(hisNumber);
+}   while (hisNumber <100)
+
+//for loop
+for (let i = 0; i<=10; i++){
+    console.log(i);
+}
+
+//while true
+let myName= "Sebastian Vettel"
+let counter = 0; //counter is a variable like index
+let myLetter;
+while (true){
+    myLetter= myName[counter];
+    console.log(myLetter);
+    if (counter ===1){
+        counter+=2;
+        continue;//tells the loop to start from the top again
+    }
+    if (myLetter ==="l") break; 
+    counter++;
+}
+console.log(counter);
---
+++ Javascript12/summary.md
@@ -0,0 +1,6 @@
+Learnt about for loops, while loops, do continue, break
+for a (condition) do this {}
+for (){}
+while this{do this}
+do {inside} - this runs the code atleast one time
+while true runs the code while the condition is true and if there is a continue nested inside of a loop continue;//tells the loop to start from the top again
